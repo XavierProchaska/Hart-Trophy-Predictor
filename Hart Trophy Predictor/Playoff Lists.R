c1d513a72bc8ac70f3962a4b4f214e29affefec2
Xavier <xavierjamesp@gmail.com>
2022-11-28 15:48:45 -0800

    Playoff teams by year

---
+++ Hart Trophy Predictor/Playoff Lists.R
@@ -0,0 +1,4 @@
+playoffteams09 <- c('BOS', 'WAS', 'NJ', 'PIT', 'PHI', 'CAR', 'NYR', 'MTL',
+'SJS', 'DET', 'VAN', 'CHI', 'CGY', 'STL', 'CBJ', 'ANA')
+playoffteams22 <- c('CAR', 'NYR', 'PIT', 'FLA', 'TOR', 'TBL', 'BOS', 'WSH', 'COL', 
+'MIN', 'STL', 'CGY', 'EDM', 'LAK', 'DAL', 'NSH')
