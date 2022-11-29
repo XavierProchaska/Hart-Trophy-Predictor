9c7b4b439c4a3f27763e2169d3ffe433b7f6ff1d
Xavier <xavierjamesp@gmail.com>
2022-11-28 17:13:01 -0800

    Added rest of the playoff teams

--- Hart Trophy Predictor/Playoff Lists.R
+++ Hart Trophy Predictor/Playoff Lists.R
@@ -1,6 +1,28 @@
-playoffteams09 <- c('BOS', 'WAS', 'N.J', 'PIT', 'PHI', 'CAR', 'NYR', 'MTL',
+playoffteams09 <- c('BOS', 'WSH', 'N.J', 'PIT', 'PHI', 'CAR', 'NYR', 'MTL',
 'SJS', 'DET', 'VAN', 'CHI', 'CGY', 'STL', 'CBJ', 'ANA')
-playoffteams10 <- c('WAS', 'N.J', 'BUF', 'PIT', 'OTT', 'BOS', 'PHI', 'MTL', 
+playoffteams10 <- c('WSH', 'N.J', 'BUF', 'PIT', 'OTT', 'BOS', 'PHI', 'MTL', 
 'SJS', 'CHI', 'DET', 'VAN', 'PHX', 'LAK', 'NSH', 'COL')
+playoffteams11 <- c('WSH', 'PHI', 'BOS', 'PIT', 'TBL', 'MTL', 'BUF', 'NYR', 
+'VAN', 'SJS', 'DET', 'ANA', 'NSH', 'PHX', 'LAK', 'CHI')
+playoffteams12 <- c('NYR', 'BOS', 'FLA', 'PIT', 'PHI', 'N.J', 'WSH', 'OTT', 
+'VAN', 'STL', 'PHX', 'NSH', 'DET', 'CHI', 'SJS', 'LAK')
+playoffteams13 <- c('PIT', 'MTL', 'WSH', 'BOS', 'TOR', 'NYR', 'OTT', 'NYI', 
+'CHI', 'ANA', 'VAN', 'STL', 'LAK', 'SJS', 'DET', 'MIN')
+playoffteams14 <- c('PIT', 'NYR', 'PHI', 'BOS', 'TBL', 'MTL', 'CBJ', 'DET', 
+'COL', 'STL', 'CHI', 'ANA', 'SJS', 'LAK', 'MIN', 'DAL')
+playoffteams15 <- c('NYR', 'WSH', 'NYI', 'MTL', 'TBL', 'DET', 'OTT', 'PIT', 
+'STL', 'NSH', 'CHI', 'ANA', 'VAN', 'CGY', 'MIN', 'WPG')
+playoffteams16 <- c('WSH', 'PIT', 'NYR', 'FLA', 'TBL', 'DET', 'NYI', 'PHI', 
+'DAL', 'STL', 'CHI', 'ANA', 'LAK', 'SJS', 'NSH', 'MIN')
+playoffteams17 <- c('WSH', 'PIT', 'CBJ', 'MTL', 'OTT', 'BOS', 'NYR', 'TOR', 
+'CHI', 'MIN', 'STL', 'ANA', 'EDM', 'SJS', 'CGY', 'NSH')
+playoffteams18 <- c('WSH', 'PIT', 'PHI', 'TBL', 'BOS', 'TOR', 'CBJ', 'N.J', 
+'NSH', 'WPG', 'MIN', 'VGK', 'ANA', 'SJS', 'LAK', 'COL')
+playoffteams19 <- c('WSH', 'NYI', 'PIT', 'TBL', 'BOS', 'TOR', 'CAR', 'CBJ', 
+'NSH', 'WPG', 'STL', 'CGY', 'SJS', 'VGK', 'DAL', 'COL')
+playoffteams20 <- c('PHI', 'WSH', 'NYI', 'BOS', 'TBL', 'MTL', 'CBJ', 'CAR', 
+'VGK', 'CHI', 'COL', 'ARI', 'DAL', 'CGY', 'STL', 'VAN')
+playoffteams21 <- c('COL', 'VGK', 'MIN', 'STL', 'CAR', 'FLA', 'TBL', 'NSH', 
+'PIT', 'WSH', 'BOS', 'NYI', 'TOR', 'EDM', 'WPG', 'MTL')
 playoffteams22 <- c('CAR', 'NYR', 'PIT', 'FLA', 'TOR', 'TBL', 'BOS', 'WSH', 'COL', 
 'MIN', 'STL', 'CGY', 'EDM', 'LAK', 'DAL', 'NSH')
