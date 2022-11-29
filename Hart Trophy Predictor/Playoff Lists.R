9a837879ca05456653000ca1c4a581e742ea0e8d
Xavier <xavierjamesp@gmail.com>
2022-11-28 16:11:03 -0800

    added 2010 teams

--- Hart Trophy Predictor/Playoff Lists.R
+++ Hart Trophy Predictor/Playoff Lists.R
@@ -1,4 +1,6 @@
-playoffteams09 <- c('BOS', 'WAS', 'NJ', 'PIT', 'PHI', 'CAR', 'NYR', 'MTL',
+playoffteams09 <- c('BOS', 'WAS', 'N.J', 'PIT', 'PHI', 'CAR', 'NYR', 'MTL',
 'SJS', 'DET', 'VAN', 'CHI', 'CGY', 'STL', 'CBJ', 'ANA')
+playoffteams10 <- c('WAS', 'N.J', 'BUF', 'PIT', 'OTT', 'BOS', 'PHI', 'MTL', 
+'SJS', 'CHI', 'DET', 'VAN', 'PHX', 'LAK', 'NSH', 'COL')
 playoffteams22 <- c('CAR', 'NYR', 'PIT', 'FLA', 'TOR', 'TBL', 'BOS', 'WSH', 'COL', 
 'MIN', 'STL', 'CGY', 'EDM', 'LAK', 'DAL', 'NSH')
