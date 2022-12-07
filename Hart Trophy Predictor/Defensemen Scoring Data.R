a803c09b8c2bad122fe62b256cf720bd8b84bc0f
Xavier <xavierjamesp@gmail.com>
2022-12-07 10:40:12 -0800

    began collecting dmen scoring data

---
+++ Hart Trophy Predictor/Defensemen Scoring Data.R
@@ -0,0 +1,6 @@
+hart_position09 <- c(8, 13, 17, 24)
+dmen09 <- c('Zdeno Chara', 'Mike Green', 'Nicklas Lidstrom', 'Dan Boyle')
+d_goals09 <- c(19, 31, 16, 16)
+d_assists09 <- c(31, 42, 43, 41)
+d_points09 <- d_goals09 + d_assists09
+d_playoffs09 <- c('Yes', 'Yes', 'Yes', 'Yes')
