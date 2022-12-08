c4bd4c0d9dd77f3dc02ca1ff4a95c26abb9c61ba
Xavier <xavierjamesp@gmail.com>
2022-12-08 13:18:17 -0800

    added data for 2010, 11, 12, 13, 14, 15

--- Hart Trophy Predictor/Defensemen Scoring Data.R
+++ Hart Trophy Predictor/Defensemen Scoring Data.R
@@ -7,3 +7,57 @@ d_playoffs09 <- c('Yes', 'Yes', 'Yes', 'Yes')
 dmen_scoring_data09 <- data.frame(hart_position09, dmen09, d_goals09, d_assists09, d_points09, d_playoffs09)
 colnames(dmen_scoring_data09) <- c('Pos', 'Player', 'G', 'A', 'PTS', 'Playoffs?')
 
+hart_position10 <- c(14, 15, 15, 17, 17, 23, 23)
+dmen10 <- c('Mike Green', 'Zdeno Chara', 'Duncan Keith', 'Drew Doughty', 'Nicklas Lidstrom', 'Chris Pronger', 'Shea Weber')
+d_goals10 <- c(19, 7, 14, 16, 9, 10, 16)
+d_assists10 <- c(57, 37, 55, 43, 40, 45, 27)
+d_points10 <- d_goals10 + d_assists10
+d_playoffs10 <- c('Yes', 'Yes', 'Yes', 'Yes', 'Yes', 'Yes', 'Yes')
+dmen_scoring_data10 <- data.frame(hart_position10, dmen10, d_goals10, d_assists10, d_points10, d_playoffs10)
+colnames(dmen_scoring_data10) <- c('Pos', 'Player', 'G', 'A', 'PTS', 'Playoffs?')
+
+hart_position11 <- c(13, 19, 20, 23, 23, 25, 26)
+dmen11 <- c('Zdeno Chara', 'Nicklas Lidstrom', 'Kris Letang', 'Chris Pronger', 'Lubomir Visnovsky', 'Shea Weber', 'Keith Yandle')
+d_goals11 <- c(14, 16, 8, 4, 18, 16, 11)
+d_assists11 <- c(30, 46, 42, 21, 50, 32, 48)
+d_points11 <- d_goals11 + d_assists11
+d_playoffs11 <- c('Yes', 'Yes', 'Yes', 'Yes', 'Yes', 'Yes', 'Yes')
+dmen_scoring_data11 <- data.frame(hart_position11, dmen11, d_goals11, d_assists11, d_points11, d_playoffs11)
+colnames(dmen_scoring_data11) <- c('Pos', 'Player', 'G', 'A', 'PTS', 'Playoffs?')
+
+hart_position12 <- c(8, 12, 13, 20)
+dmen12 <- c('Erik Karlsson', 'Zdeno Chara', 'Shea Weber', 'Brian Campbell')
+d_goals12 <- c(19, 12, 19, 4)
+d_assists12 <- c(59, 40, 30, 49)
+d_points12 <- d_goals12 + d_assists12
+d_playoffs12 <- c('Yes', 'Yes', 'Yes', 'Yes')
+dmen_scoring_data12 <- data.frame(hart_position12, dmen12, d_goals12, d_assists12, d_points12, d_playoffs12)
+colnames(dmen_scoring_data12) <- c('Pos', 'Player', 'G', 'A', 'PTS', 'Playoffs?')
+
+hart_position13 <- c(23, 28, 32)
+dmen13 <- c('P.K. Subban', 'Ryan Suter', 'Francois Beauchemin')
+d_goals13 <- c(11, 4, 6)
+d_assists13 <- c(27, 28, 18)
+d_points13 <- d_goals13 + d_assists13
+d_playoffs13 <- c('Yes', 'Yes', 'Yes')
+dmen_scoring_data13 <- data.frame(hart_position13, dmen13, d_goals13, d_assists13, d_points13, d_playoffs13)
+colnames(dmen_scoring_data13) <- c('Pos', 'Player', 'G', 'A', 'PTS', 'Playoffs?')
+
+hart_position14 <- c(15, 21, 23)
+dmen14 <- c('Duncan Keith', 'Shea Weber', 'Ryan Suter')
+d_goals14 <- c(6, 23, 8)
+d_assists14 <- c(55, 33, 35)
+d_points14 <- d_goals14 + d_assists14
+d_playoffs14 <- c('Yes', 'No', 'Yes')
+dmen_scoring_data14 <- data.frame(hart_position14, dmen14, d_goals14, d_assists14, d_points14, d_playoffs14)
+colnames(dmen_scoring_data14) <- c('Pos', 'Player', 'G', 'A', 'PTS', 'Playoffs?')
+
+hart_position15 <- c(9, 16, 17, 18, 18)
+dmen15 <- c('Erik Karlsson', 'P.K. Subban', 'Drew Doughty', 'Dustin Byufglien', 'Shea Weber')
+d_goals15 <- c(21, 15, 7, 18, 15)
+d_assists15 <- c(45, 45, 39, 27, 30)
+d_points15 <- d_goals15 + d_assists15
+d_playoffs15 <- c('Yes', 'Yes', 'No', 'Yes', 'Yes')
+dmen_scoring_data15 <- data.frame(hart_position15, dmen15, d_goals15, d_assists15, d_points15, d_playoffs15)
+colnames(dmen_scoring_data15) <- c('Pos', 'Player', 'G', 'A', 'PTS', 'Playoffs?')
+
