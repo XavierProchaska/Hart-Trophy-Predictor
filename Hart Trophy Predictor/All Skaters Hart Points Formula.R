c62e94422bc4196412af1c7de7619de1a9a48724
Xavier <xavierjamesp@gmail.com>
2023-01-05 15:32:26 -0800

    combined forward and defensemen formulas

--- Hart Trophy Predictor/All Skaters Hart Points Formula.R
+++ Hart Trophy Predictor/All Skaters Hart Points Formula.R
@@ -1,3 +1,53 @@
 skaters_hart_points <- function(raw_data, playoffs) {
-    return(raw_data)
+    new_data <- read.csv(raw_data)
+    new_data <- subset(new_data, situation == 'all', select = c(name, team, position, I_F_points, I_F_goals))
+
+    hart <- c()
+    for (i in 1:nrow(new_data)) {
+       hart <- c(hart, new_data$I_F_points[i])
+    }
+
+    for (i in 1:nrow(new_data)) {
+       if (toString(new_data$team[i]) %in% playoffs) {
+          hart[i] <- hart[i] + 10
+       }
+       else {
+          hart[i] <- hart[i] - 5
+       }
+    }
+    new_data$hart_points <- hart
+
+    for (i in 1:nrow(new_data)) {
+       if (new_data$I_F_goals[i] >= 50 && new_data$I_F_points[i] >= 80) {
+          new_data$hart_points[i] <- new_data$hart_points[i] + 20
+       }
+    }
+
+    for (i in 1:nrow(new_data)) {
+       if (new_data$position[i] == 'D') {
+          if (new_data$I_F_points[i] >= 90) {
+             new_data$hart_points[i] <- new_data$hart_points[i] + 15
+          }
+          else if (new_data$I_F_points[i] >= 80) {
+             new_data$hart_points[i] <- new_data$hart_points[i] + 10
+          }
+          else if (new_data$I_F_points[i] >= 70) {
+             new_data$hart_points[i] <- new_data$hart_points[i] + 5
+          }
+       }
+    }
+    new_data <- new_data[order(new_data$hart_points, decreasing = TRUE),]
+
+    for (i in 1:5) {
+       for (j in 1:5) {
+          if (i != j) {
+             if (new_data$team[i] == new_data$team[j]) {
+                new_data$hart_points[i] <- new_data$hart_points[i] - 10
+             }
+          }
+       }
+    }
+    new_data <- new_data[order(new_data$hart_points, decreasing = TRUE),]
+    return(head(new_data, n = 10L))
 }
+skaters_hart_points('/home/xavier/Documents/Hart Trophy Predictor/Raw Data/skaters_21-22.csv', playoffteams22)
