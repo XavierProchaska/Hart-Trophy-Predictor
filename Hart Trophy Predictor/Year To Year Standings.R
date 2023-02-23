a27193a754170717da22ef41670cb9d45493e361
Xavier <xavierjamesp@gmail.com>
2023-02-23 13:51:47 -0800

    added standings for 2009-2022

--- Hart Trophy Predictor/Year To Year Standings.R
+++ Hart Trophy Predictor/Year To Year Standings.R
@@ -1,1 +1,42 @@
-standings2009 <- c()
+standings2009 <- c('SJS', 'BOS', 'DET', 'WSH', 'NJD', 'CHI', 'VAN', 'PIT', 'PHI',
+'CGY', 'CAR', 'NYR', 'MTL', 'FLA', 'STL', 'CBJ', 'ANA', 'BUF', 'MIN', 'NSH', 'EDM',
+'OTT', 'DAL', 'TOR', 'PHX', 'LAK', 'ATL', 'COL', 'TBL', 'NYI')
+standings2010 <- c('WSH', 'SJS', 'CHI', 'PHX', 'VAN', 'NJD', 'DET', 'PIT', 'LAK',
+'NSH', 'BUF', 'COL', 'OTT', 'BOS', 'STL', 'CGY', 'ANA', 'PHI', 'MTL', 'DAL', 'NYR',
+'MIN', 'ATL', 'CAR', 'TBL', 'NYI', 'CBJ', 'FLA', 'TOR', 'EDM')
+standings2011 <- c('VAN', 'WSH', 'PHI', 'PIT', 'SJS', 'DET', 'BOS', 'TBL', 'ANA',
+'NSH', 'PHX', 'LAK', 'CHI', 'MTL', 'BUF', 'DAL', 'CGY', 'NYR', 'CAR', 'STL', 'MIN',
+'TOR', 'NJD', 'CBJ', 'ATL', 'OTT', 'NYI', 'FLA', 'COL', 'EDM')
+standings2012 <- c('VAN', 'NYR', 'STL', 'PIT', 'NSH', 'PHI', 'BOS', 'DET', 'NJD',
+'CHI', 'PHX', 'SJS', 'LAK', 'FLA', 'WSH', 'OTT', 'CGY', 'DAL', 'BUF', 'COL', 'TBL',
+'WPG', 'CAR', 'MIN', 'ANA', 'TOR', 'NYI', 'MTL', 'EDM', 'CBJ')
+standings2013 <- c('CHI', 'PIT', 'ANA', 'MTL', 'BOS', 'STL', 'LAK', 'VAN', 'TOR',
+'WSH', 'SJS', 'NYR', 'DET', 'OTT', 'MIN', 'NYI', 'CBJ', 'WPG', 'PHX', 'PHI', 'DAL',
+'NJD', 'BUF', 'EDM', 'CGY', 'CAR', 'NSH', 'TBL', 'COL', 'FLA')
+standings2014 <- c('BOS', 'ANA', 'COL', 'STL', 'SJS', 'PIT', 'CHI', 'TBL', 'MTL',
+'LAK', 'MIN', 'NYR', 'PHI', 'CBJ', 'DET', 'DAL', 'WSH', 'PHX', 'NSH', 'NJD', 'OTT',
+'WPG', 'TOR', 'CAR', 'VAN', 'NYI', 'CGY', 'EDM', 'FLA', 'BUF')
+standings2015 <- c('NYR', 'MTL', 'ANA', 'STL', 'TBL', 'NSH', 'CHI', 'VAN', 'WSH',
+'NYI', 'MIN', 'DET', 'OTT', 'WPG', 'PIT', 'CGY', 'BOS', 'LAK', 'DAL', 'FLA', 'COL',
+'SJS', 'CBJ', 'PHI', 'NJD', 'CAR', 'TOR', 'EDM', 'ARI', 'BUF')
+standings2016 <- c('WSH', 'DAL', 'STL', 'PIT', 'CHI', 'ANA', 'FLA', 'LAK', 'NYR',
+'NYI', 'SJS', 'TBL', 'PHI', 'NSH', 'DET', 'BOS', 'MIN', 'CAR', 'OTT', 'NJD', 'COL',
+'MTL', 'BUF', 'ARI', 'WPG', 'CGY', 'CBJ', 'VAN', 'EDM', 'TOR')
+standings2017 <- c('WSH', 'PIT', 'CHI', 'CBJ', 'MIN', 'ANA', 'MTL', 'EDM', 'NYR',
+'STL', 'SJS', 'OTT', 'BOS', 'TOR', 'CGY', 'NSH', 'NYI', 'TBL', 'PHI', 'WPG', 'CAR',
+'LAK', 'FLA', 'DAL', 'DET', 'BUF', 'NJD', 'ARI', 'VAN', 'COL')
+standings2018 <- c('NSH', 'WPG', 'TBL', 'BOS', 'VGK', 'WSH', 'TOR', 'MIN', 'ANA',
+'PIT', 'SJS', 'LAK', 'PHI', 'CBJ', 'NJD', 'FLA', 'COL', 'STL', 'DAL', 'CGY', 'CAR',
+'NYI', 'EDM', 'NYR', 'CHI', 'VAN', 'DET', 'MTL', 'ARI', 'OTT', 'BUF')
+standings2019 <- c('TBL', 'CGY', 'BOS', 'WSH', 'NYI', 'SJS', 'TOR', 'NSH', 'PIT',
+'WPG', 'CAR', 'STL', 'CBJ', 'MTL', 'DAL', 'VGK', 'COL', 'ARI', 'FLA', 'CHI', 'MIN',
+'PHI', 'VAN', 'ANA', 'EDM', 'NYR', 'BUF', 'DET', 'NJD', 'LAK', 'OTT')
+standings2020 <- c('BOS', 'STL', 'COL', 'TBL', 'WSH', 'PHI', 'PIT', 'VGK', 'CAR',
+'DAL', 'NYI', 'EDM', 'TOR', 'CBJ', 'FLA', 'NSH', 'VAN', 'NYR', 'CGY', 'WPG', 'MIN',
+'ARI', 'CHI', 'MTL', 'BUF', 'NJD', 'ANA', 'LAK', 'SJS', 'OTT', 'DET')
+standings2021 <- c('COL', 'VGK', 'CAR', 'FLA', 'PIT', 'TOR', 'WSH', 'TBL', 'MIN',
+'BOS', 'EDM', 'NYI', 'NSH', 'WPG', 'STL', 'NYR', 'DAL', 'MTL', 'PHI', 'CGY', 'CHI',
+'ARI', 'OTT', 'VAN', 'LAK', 'SJS', 'DET', 'CBJ', 'NJD', 'ANA', 'BUF')
+standings2022 <- c('FLA', 'COL', 'CAR', 'TOR', 'MIN', 'CGY', 'NYR', 'TBL', 'STL',
+'BOS', 'EDM', 'PIT', 'WSH', 'LAK', 'DAL', 'NSH', 'VGK', 'VAN', 'WPG', 'NYI', 'CBJ',
+'SJS', 'ANA', 'BUF', 'DET', 'OTT', 'CHI', 'NJD', 'PHI', 'SEA', 'ARI', 'MTL')
