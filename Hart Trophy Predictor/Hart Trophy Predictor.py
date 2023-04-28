98695aa580623d26d3464c6d188ffec598473677
Xavier <xavierjamesp@gmail.com>
2023-04-28 11:52:47 -0700

    started goaltender code

--- Hart Trophy Predictor/Hart Trophy Predictor.py
+++ Hart Trophy Predictor/Hart Trophy Predictor.py
@@ -18,5 +18,10 @@ def hart_trophy_predictor(skater_data, goalie_data, playoffs):
                                                & (hart_skater_data['I_F_points'] >= 70), hart_skater_data['hart_points'] + 5,
                                                  hart_skater_data['hart_points'])
     hart_skater_data = hart_skater_data.sort_values(by=['hart_points'], ascending=False)
-    return(hart_skater_data.head(15))
-print(hart_trophy_predictor('/home/xavier/Documents/Git Repositories/Hart-Trophy-Predictor/Hart Trophy Predictor/Raw Data/Skater Data/skaters_21-22.csv',0,0))
+
+    hart_goaltender_data = pd.read_excel(goalie_data, sheet_name='QuantHockey')
+    hart_goaltender_data = hart_goaltender_data.loc[['Name', 'Position', 'Goals', 'Points', 'GAA', 'SV%', 'W', 'SO']]
+
+    return(hart_goaltender_data.head())
+print(hart_trophy_predictor('/home/xavier/Documents/Git Repositories/Hart-Trophy-Predictor/Hart Trophy Predictor/Raw Data/Skater Data/skaters_21-22.csv',
+                            '/home/xavier/Documents/Git Repositories/Hart-Trophy-Predictor/Hart Trophy Predictor/Raw Data/Goalie Data/goalies_22-23.xlsx',0))
